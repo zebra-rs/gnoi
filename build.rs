e0b6a6e71d7cb39ae3e0e36628f97b117761e222
Kunihiro Ishiguro <ishi@hash-set.com>
2020-02-16 05:32:32 -0800

    Add proto files.

---
+++ build.rs
@@ -0,0 +1,9 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure().build_server(true).compile(
+        &[
+            "./bgp/bgp.proto",
+        ],
+        &["./bgp/"],
+    )?;
+    Ok(())
+}
